a3036f8bf75effb77ea5979b43decf2f207bb299
Mo <mort.saghi@gmail.com>
2023-06-12 18:42:59 +0330

    All Done

--- grade cal.py
+++ grade cal.py
@@ -1,21 +1,17 @@
-# # to examine the grade
-# def gradeReviewTeller(grade):
-#     if grade >= 17:
-#         print("Great")
-#     elif 12 < grade < 17:
-#         print("Normal")
-#     elif 12 > grade:
-#         print("Fail")
+# to examine the grade
+def gradeReviewTeller(grade):
+    if grade >= 17:
+        print("Great")
+    elif 12 < grade < 17:
+        print("Normal")
+    elif 12 > grade:
+        print("Fail")
 
 
-def GPA(A):
-    grades = list()
-    isfinished = False
-    while isfinished == False:
-        theinpute = int(
-            input("please enter the grade\n"))
-        grades.append(theinpute)
-        print(grades)
+sumOfGrades = 0
+howmanygrades = int(input("how many grades do you want to enter"))
+for i in range(howmanygrades):
+    a = int(input("plz enter your grade "))
+    sumOfGrades += a
 
-
-GPA("A")
+gradeReviewTeller(sumOfGrades / howmanygrades)
